dbd548bad3824dbc9654ae81417fa6aea68151e7
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-02-21 00:02:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,18 +4,18 @@
 
 
 bool checkPrime(uint64_t value) {
-	for (uint64_t i = 2; i <= sqrt(value); i++) {
-		if (value % i == 0)
+    for (uint64_t i = 2; i <= sqrt(value); i++) {
+	    if (value % i == 0)
             return false;
     }
     return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t curNum = 2;
+    uint64_t curNum = 2;
     uint64_t i = 0;
     while (1) {
-		if (checkPrime(curNum) == 1) {
+        if (checkPrime(curNum) == 1) {
             i++;
         }
         if (i == n) {
@@ -38,9 +38,9 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t curNum= 2;
+	uint64_t curNum = 2;
     uint64_t sum = 0;
-    while (curNum<hbound) {
+    while (curNum < hbound) {
     	if (checkPrime(curNum)) {
             sum += curNum;
             curNum++;
