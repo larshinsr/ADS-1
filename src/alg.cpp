09b4b2d2419ef7e09d6c801f217d1d428c2fedec
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-02-20 23:47:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,50 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	for (uint64_t i = 2; i <= sqrt(value); i++) {
+		if (value % i == 0)
+            return false;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t curNum = 2;
+    uint64_t i = 0;
+    while (1) {
+		if (checkPrime(curNum) == 1) {
+            i++;
+        }
+        if (i == n) {
+            break;
+        }
+        ++curNum;
+    }
+    return curNum;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	uint64_t nextPr = 0;
+    while (1) {
+        value++;
+        if (checkPrime(value)) {
+            return value;
+            break;
+        }
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t curNum= 2;
+    uint64_t sum = 0;
+    while (curNum<hbound) {
+    	if (checkPrime(curNum)) {
+            sum += curNum;
+            curNum++;
+        }
+        else {
+            curNum++;
+		}
+    }
+    return sum;
 }
